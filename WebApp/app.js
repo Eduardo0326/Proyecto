304259dce3f9b392839a2a19a02c14f5bb6e3fda
Eduardo0326 <47803985+Eduardo0326@users.noreply.github.com>
2020-04-24 11:49:27 -0500

    proyecto integrador
    Login del proyecto integrador, autentificacion con google y facebook.

---
+++ WebApp/app.js
@@ -0,0 +1,39 @@
+var providerGoogle   = new firebase.auth.GoogleAuthProvider();
+
+// jquery sujeta la etiqueta root
+$('#glogin').click(function(){
+  firebase.auth().signInWithPopup(providerGoogle)
+     .then(function(result){
+      console.log(result.user);
+      $('#glogin').hide();
+      guardarDatos(result.user);
+      $('#root').append( result.user.displayName);
+      $('#avatar').attr('src',result.userPhotoURL) 
+   
+      });
+});
+
+var providerFacebook = new firebase.auth.FacebookAuthProvider();
+
+$('#flogin').click(function(){
+  firebase.auth().
+  signInWithPopup(providerFacebook)
+     .then(function(result){
+      console.log(result.user);
+      $('#flogin').hide();
+      guardarDatos(result.user);
+      $('#root').append( result.user.displayName);
+      $('#avatar').attr('src',result.userPhotoURL) 
+      });
+});
+
+function guardarDatos(user){
+  var usuarios= {
+      uid     :user.uid,
+      nombre  :user.displayName,
+      email   :user.email,
+      foto    :user.photoURL
+  }
+  firebase.database().ref("taller05/Usuarios/"+user.uid).set(usuarios)
+
+}
